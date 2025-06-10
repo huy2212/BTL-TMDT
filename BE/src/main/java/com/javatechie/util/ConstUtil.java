77835f8993f181352158e2307b7c97d347fb0e50
Nguyễn Quang Huy <99111312+huy2212@users.noreply.github.com>
2025-06-10 17:03:17 +0700

    Delete BE directory

--- BE/src/main/java/com/javatechie/util/ConstUtil.java
+++
@@ -1,8 +0,0 @@
-package com.javatechie.util;
-
-public class ConstUtil {
-
-    public static final String ROLE_EMPLOYEE = "EMPLOYEE";
-    public static final String ROLE_ADMIN = "ADMIN";
-    public static final String ROLE_USER = "USER";
-}
