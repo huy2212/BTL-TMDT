3ad3ea2d65be5f7cf4f621e066b1a809f97520d4
Nguyễn Quang Huy <99111312+huy2212@users.noreply.github.com>
2025-06-10 17:03:43 +0700

    Delete FE directory

--- FE/src/config/const.js
+++
@@ -1,3 +0,0 @@
-const IP = "http://localhost:8081"
-const token ="eyJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJhZG1pbjEiLCJpYXQiOjE3MTYzMDUwMzYsImV4cCI6MTcxNjMwODYzNn0.sromxDskij5mgHORYo9cEqNiHsIWErR5PofrNXUwVHY"
-export {IP,token}
--- FE/src/routes/Routes.js
+++
@@ -1,9 +0,0 @@
-import HomePage from "../pages/HomePage.jsx";
-import LoginPage from "../pages/LoginPage.jsx";
-import SignupPage from "../pages/SignupPage.jsx";
-import ShopsPage from "../pages/ShopsPage.jsx";
-import ProductDetailPage from "../pages/ProductDetailPage.jsx";
-import CartPage from "../pages/CartPage.jsx";
-import ProductListPage from "../pages/ProductListPage.jsx"
-import UserAccountPage from "../pages/UserAccountPage.jsx";
-export { HomePage, LoginPage, SignupPage, ShopsPage, ProductDetailPage, CartPage, ProductListPage, UserAccountPage };
